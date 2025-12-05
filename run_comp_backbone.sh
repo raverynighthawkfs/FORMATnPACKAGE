dab11ce0ba377ea685e83b199f183c4ba11ef954
Richard Avery <ravery@nighthawkfs.com>
2025-12-05 14:48:41 -0800

    added cli command on sh format for powershell users

---
+++ run_comp_backbone.sh
@@ -0,0 +1,4 @@
+python compression_backbone.py "C:\Users\ravery\Desktop\test_files" ^
+  --out-dir "C:\Users\ravery\Desktop\compression_results" ^
+  --ext .tif .tiff .png .jpg .json .txt ^
+  --max-files 50
